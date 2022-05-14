d7fcdb35486e766314a6e2a337c41909a07ce9b9
satoshimeow <satoshi.meow@gmail.com>
2022-05-14 14:00:00 -0700

    Adds a NFT smart contract by following shorturl.at/bcpHN

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+const { API_URL, PRIVATE_KEY } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +21,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {},
+    ropsten: {
+       url: API_URL,
+       accounts: [`0x${PRIVATE_KEY}`]
+    }
+ },
 };
